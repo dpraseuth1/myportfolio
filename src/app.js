bd251dde9c4d749723a56d9e254f570f966a3cb4
David Praseuth <davidrpraseuth@gmail.com>
2023-04-06 21:44:05 -0700

    added contact form with responsive text

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const barsButton = document.getElementById('bars');
+const navList = document.getElementById('nav-list');
+
+function toggleButton() {
+  navList.classList.toggle('show');
+}
+
+barsButton.addEventListener('click', toggleButton);
