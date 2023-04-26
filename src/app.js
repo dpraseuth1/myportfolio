e52e092ad4d059ea9f6522f23629a08ad4965a90
David Praseuth <davidrpraseuth@gmail.com>
2023-04-25 22:07:21 -0700

    added css media queries

--- src/app.js
+++ src/app.js
@@ -1,22 +1,8 @@
-const barsButton = document.getElementById('bars');
-const navList = document.getElementById('nav-list');
+const barsButton = document.getElementById("bars");
+const navList = document.getElementById("nav-list");
 
 function toggleButton() {
-  navList.classList.toggle('show');
+  navList.classList.toggle("show");
 }
 
-barsButton.addEventListener('click', toggleButton);
-
-// dark mode
-
-const toggleSwitch = document.querySelector('#night-mode-toggle');
-
-toggleSwitch.addEventListener('change', switchTheme, false);
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.body.classList.add('dark-mode');
-  } else {
-    document.body.classList.remove('dark-mode');
-  }
-}
+barsButton.addEventListener("click", toggleButton);
